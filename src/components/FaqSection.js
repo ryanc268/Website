72459d710db9c53f7d60c8f451835ffdbc7f780d
Ryan Coppa <ryan.tcoppa@gmail.com>
2022-01-06 02:49:41 -0500

    FAQ Toggle component

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,6 +2,8 @@ import React from "react";
 //Styles
 import styled from "styled-components";
 import { About } from "../styles";
+//Components
+import Toggle from "./Toggle";
 
 const FaqSection = () => {
   return (
@@ -9,46 +11,57 @@ const FaqSection = () => {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Earum, tempore?
-          </p>
+      <Toggle>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Earum, tempore?
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Earum, tempore?
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Earum, tempore?
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Earum, tempore?
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Different Payment Methods</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Earum, tempore?
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Products do you offer?</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Earum, tempore?
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>What Products do you offer?</h4>
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Earum, tempore?
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
     </Faq>
   );
 };
