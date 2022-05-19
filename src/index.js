0ebdcf15eaadc724aadefcd9be6981888a9014e2
Ryan Coppa <ryan.tcoppa@gmail.com>
2022-05-19 18:05:22 -0400

    Removing GH Pages and Switched back to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
